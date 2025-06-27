7c1c6a0dd5d8537ead120334ae8f8d5c62b62a9f
Rishang <rishangkumar9693@gmail.com>
2025-06-27 13:44:15 +0530

    now it can upload and get data from the file

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,66 @@
+"use client";
+import { useState } from "react";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
-import Image from "next/image";
+import Papa from "papaparse";
+import { file } from "zod/v4";
 
 export default function Home() {
+  const [files, setFiles] = useState<{
+    client: File | null;
+    worker: File | null;
+    task: File | null;
+  }>({
+    client: null,
+    worker: null,
+    task: null,
+  });
+
+  const parseCSV = (file: File): Promise<any[]> => {
+    return new Promise((resolve, reject) => {
+      Papa.parse(file, {
+        header: true,
+        skipEmptyLines: true,
+        complete: (results: any) => resolve(results.data),
+        error: (err: any) => reject(err),
+      });
+    });
+  };
+
+  const handlesubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+
+    const { client, worker, task } = files;
+
+    if (!client || !worker || !task) {
+      alert("uplaod all the files pls");
+      return;
+    }
+    //so now get all the file parse it and save it in localstorage for now
+    try {
+      const [clients, workers, tasks] = await Promise.all([
+        parseCSV(client),
+        parseCSV(worker),
+        parseCSV(task),
+      ]);
+
+      console.log(clients, "clientdata");
+      console.log(workers, "workerdata");
+      console.log(tasks, "parsedata");
+
+      alert("Your  file is uplaoded ");
+
+      setFiles({
+        client: null,
+        worker: null,
+        task: null,
+      });
+    } catch (error) {
+      alert("something went wrong");
+    }
+  };
+
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center relative   justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <div className="absolute bg-slate-900 inset-0 -z-10   bg-[size:4rem_4rem] bg-[linear-gradient(to_right,#222_1px,transparent_1px),linear-gradient(to_bottom,#222_1px,transparent_1px)]"></div>
@@ -17,44 +74,71 @@ export default function Home() {
           Upload your Files here
         </h1>
         <div className="flex  p-2  items-center justify-center">
-          <form action=""  className="flex flex-col items-center p-2">
+          <form
+            onSubmit={handlesubmit}
+            className="flex flex-col items-center p-2"
+          >
             <div className="space-y-2 p-2">
               <Label htmlFor="picture" className="text-white">
                 Client File here
               </Label>
               <Input
-                id="picture"
                 type="file"
-                className="placeholder:text-white text-white"  
+                className="placeholder:text-white text-white"
+                onChange={(e) =>
+                  setFiles((prev) => ({
+                    ...prev,
+                    client: e.target.files?.[0] || null,
+                  }))
+                }
                 accept=".csv"
                 required
               />
             </div>
-            <div  className="space-y-2 p-2">
+            <div className="space-y-2 p-2">
               <Label htmlFor="picture" className="text-white">
                 Worker file here
               </Label>
               <Input
                 id="picture"
                 type="file"
-                className=" placeholder:text-white text-white"  
+                className=" placeholder:text-white text-white"
+                onChange={(e) =>
+                  setFiles((prev) => ({
+                    ...prev,
+                    worker: e.target.files?.[0] || null,
+                  }))
+                }
                 accept=".csv"
                 required
               />
             </div>
-            <div  className="space-y-2 p-2">
-              <Label htmlFor="picture" className="text-white" >
+            <div className="space-y-2 p-2">
+              <Label htmlFor="picture" className="text-white">
                 Task file here{" "}
               </Label>
               <Input
                 id="picture"
-                type="file" 
+                type="file"
                 required
-                className="placeholder:text-white text-white" 
+                onChange={(e) =>
+                  setFiles((prev) => ({
+                    ...prev,
+                    task: e.target.files?.[0] || null,
+                  }))
+                }
+                className="placeholder:text-white text-white"
                 accept=".csv"
               />
             </div>
-            <Button type="submit" variant={'outline'} className="cursor-pointer"> Uplaod ur file </Button>
+            <Button
+              type="submit"
+              variant={"outline"}
+              className="cursor-pointer"
+            >
+              {" "}
+              Uplaod ur file{" "}
+            </Button>
           </form>
         </div>
       </div>
